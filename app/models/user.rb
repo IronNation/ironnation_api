55620b3e3a36ed8d361185ad823333d33ec968e6
Brian Dorton <dorton@gmail.com>
2016-12-20 15:30:18 -0600

    updates forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
         user.avatar_url = auth.extra.raw_info.avatar_url
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
+        
       end
     end
 
