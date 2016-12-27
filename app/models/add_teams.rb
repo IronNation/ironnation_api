e1559b58bcc9f84a7e2cd3cd803f53f78502b6ab
Brian Dorton <dorton@gmail.com>
2016-12-26 20:44:44 -0600

    comments out add_teams

--- app/models/add_teams.rb
+++ app/models/add_teams.rb
@@ -1,20 +1,20 @@
 class AddTeams
 
     # Sets Octokit Auth
-    octokit = Octokit::Client.new(:access_token => ENV["git_token"])
+    # octokit = Octokit::Client.new(:access_token => ENV["git_token"])
 
     # Adds Teams to Database
-    octokit.organization_teams(24231442).map{|a| [a.id, a.name]}.each do |team|
-      Team.first_or_create(name: team[1], uid: team[0])
-    end
+    # octokit.organization_teams(24231442).map{|a| [a.id, a.name]}.each do |team|
+      # Team.first_or_create(name: team[1], uid: team[0])
+    # end
 
     #Adds Users to those teams
-    octokit.organization_teams(24231442).map{|id| id.id}.each do |team_id|
-      octokit.team_members(team_id).map{|l| l.login}.each do |user_name|
-        user = User.find_by(github_username: user_name)
-        Team.find_by(uid: team_id).users << user if user.github_username.present?
-      end
-    end
+    # octokit.organization_teams(24231442).map{|id| id.id}.each do |team_id|
+      # octokit.team_members(team_id).map{|l| l.login}.each do |user_name|
+        # user = User.find_by(github_username: user_name)
+        # Team.find_by(uid: team_id).users << user if user.github_username.present?
+      # end
+    # end
 
 
 
